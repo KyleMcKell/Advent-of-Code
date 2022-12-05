2d2557a01ee0f343123e48635b01a9bf66f2912c
Kyle McKell <me@kylemckell.com>
2022-12-05 11:30:40 -0700

    pt1 of day 4

--- 2022/day4/pt1.ts
+++ 2022/day4/pt1.ts
@@ -1,5 +1,37 @@
 import { getSplitFileData } from "../../utils.ts";
 
-const splitFileData = await getSplitFileData("example");
+const splitFileData = await getSplitFileData("input");
 
-console.log(splitFileData);
+const groupedData = splitFileData.map((section) => {
+  const splitSection = section.split(",");
+  return splitSection;
+});
+
+const sortedNumberArr = [];
+
+// split each assignment into an array of numbers to find the ranges
+for (let i = 0; i < groupedData.length; i++) {
+  const mappedData = groupedData[i].map((assignment) => {
+    const splitAssignment = assignment.split("-").map((item) => Number(item));
+    return splitAssignment;
+  });
+  const sortedData = mappedData.sort((a, b) => {
+    const rangeSumA = a[1]! - a[0];
+    const rangeSumB = b[1]! - b[0];
+    const res = rangeSumB - rangeSumA;
+    return res;
+  });
+  sortedNumberArr.push(sortedData);
+}
+
+let pairs = 0;
+
+// see if the largest range has an equal or lower starting point than the shorter one
+for (let i = 0; i < sortedNumberArr.length; i++) {
+  const [largerRange, shorterRange] = sortedNumberArr[i];
+  if (largerRange[0] <= shorterRange[0] && largerRange[1] >= shorterRange[1]) {
+    pairs++;
+  }
+}
+
+console.log(pairs);
