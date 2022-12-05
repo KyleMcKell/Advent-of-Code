1e3e6542e278cd81ac1862467401a1c89ef7c212
Kyle McKell <me@kylemckell.com>
2022-12-05 10:44:09 -0700

    commenting some stuff

--- 2022/day3/pt2.ts
+++ 2022/day3/pt2.ts
@@ -4,9 +4,9 @@ const fileData = await getFileData("input");
 
 const splitFileData = fileData.split("\n");
 
+// group up the data into sets of 3
 const groupedData: string[][] = [[]];
 let groupNum = 0;
-
 for (let i = 0; i < splitFileData.length; i++) {
   if (groupNum === 3) {
     groupNum = 0;
@@ -16,23 +16,20 @@ for (let i = 0; i < splitFileData.length; i++) {
   groupNum++;
 }
 
-console.log(groupedData);
-
+// find commonalities between the groups of 3
 const result: string[] = [];
-
 for (let i = 0; i < groupedData.length; i++) {
   const group = groupedData[i];
   const groupAsArr = group.map((sack) => sack.split(""));
   const firstSack = groupAsArr[0];
   const filteredSack = firstSack.filter((firstSackItem) => {
-    return group.every((item) => item.indexOf(firstSackItem) !== -1);
+    return groupAsArr.every((item) => item.indexOf(firstSackItem) !== -1);
   });
   result.push(filteredSack[0]);
 }
 
-const resultPrioritized: number[] = [];
-
 // convert the result array to how much each item is worth in points
+const resultPrioritized: number[] = [];
 for (let i = 0; i < result.length; i++) {
   if (result[i] === result[i].toUpperCase()) {
     const priority = result[i].charCodeAt(0) - 38;
@@ -43,5 +40,6 @@ for (let i = 0; i < result.length; i++) {
   }
 }
 
+// add them all up!
 const resultSum = resultPrioritized.reduce((x, y) => x + y, 0);
 console.log(resultSum);
