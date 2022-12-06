f34ab5b126a5c10c4d60e332ee7e01aa5ceee700
Kyle McKell <me@kylemckell.com>
2022-12-05 23:01:19 -0700

    day 6, easy day

---
+++ 2022/day6/day6.test.ts
@@ -0,0 +1,23 @@
+import { assertStrictEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts"
+import { getFileData } from "../../utils.ts"
+import { findMarker } from "./index.ts"
+
+const fileData = await getFileData("example")
+
+Deno.test("Finds marker with no obstruction", () => {
+  const mock = "asdfjkl"
+  const marker = findMarker(mock, 4)
+  assertStrictEquals(4, marker)
+})
+
+Deno.test("Finds marker with 4", () => {
+  const marker = findMarker(fileData, 4)
+  const expected = 7
+  assertStrictEquals(marker, expected)
+})
+
+Deno.test("Finds marker with 14", () => {
+  const marker = findMarker(fileData, 14)
+  const expected = 19
+  assertStrictEquals(marker, expected)
+})
---
+++ 2022/day6/pt1.ts
@@ -0,0 +1,7 @@
+import { getFileData } from "../../utils.ts"
+import { findMarker } from "./index.ts"
+
+const fileData = await getFileData("input")
+
+const marker = findMarker(fileData, 4)
+console.log(marker)
