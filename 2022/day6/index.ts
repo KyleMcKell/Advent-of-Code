6a12b7e7fc153d7d5e1ef6e079e3c2840a383f03
Kyle McKell <me@kylemckell.com>
2022-12-05 23:21:24 -0700

    making sure slice doesn't get all grumpy at me

--- 2022/day6/index.ts
+++ 2022/day6/index.ts
@@ -1,7 +1,8 @@
 export function findMarker(buffer: string, markerLength: number) {
   const bufferArr = buffer.split("")
   for (let i = markerLength - 1; i < bufferArr.length; i++) {
-    const bufferSet = new Set(bufferArr.slice(i - markerLength, i))
+    const sliceStart = i - markerLength >= 0 ? i - markerLength : 0
+    const bufferSet = new Set(bufferArr.slice(sliceStart, i))
     if (bufferSet.size === markerLength) {
       return i
     }
--- 2022/day6/pt1.ts
+++ 2022/day6/pt1.ts
@@ -1,7 +1,7 @@
 import { getFileData } from "../../utils.ts"
 import { findMarker } from "./index.ts"
 
-const fileData = await getFileData("input")
+const fileData = await getFileData("example")
 
 const marker = findMarker(fileData, 4)
 console.log(marker)
